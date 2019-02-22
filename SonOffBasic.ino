360ec7f10d486acdd396a62c836cd79f64113d7e
JorinL <33031729+JorinL@users.noreply.github.com>
2019-02-22 02:39:40 +0100

    Update SonOffBasic.ino

--- SonOffBasic.ino
+++ SonOffBasic.ino
@@ -76,10 +76,13 @@ void fixWiFi() {
   // Posts every 10 seconds the state of WiFi.status(), Homie.getMqttClient().connected() and Homie.isConfigured()
   // Within this interval the connectivity is checked and logged if a problem is detected
   // Then it disconnects Wifi, if Wifi or MQTT is not connected for 1 Minute (but only if Homie is configured)
-  if (0 == WiFifix || ((millis() - WiFifix) > 10000)) {
+  if ( WiFifix == 0 || ((millis() - WiFifix) > 10000)) {
     if (Homie.isConfigured() == 1) {
-      Homie.getLogger() << "Wifi-state:" << WiFi.status() << " | MQTT-state:" << Homie.getMqttClient().connected() << " | HomieConfig-state:" << Homie.isConfigured() << endl;
-
+      float rssi = WiFi.RSSI();
+      Homie.getLogger()  << endl;
+      relayNode.setProperty("quality").send(String(rssi));
+      Homie.getLogger() << "Wifi-state:" << WiFi.status() << " | Wi-Fi signal quality:" << rssi << " | MQTT-state:" << Homie.getMqttClient().connected() << " | HomieConfig-state:" << Homie.isConfigured() << endl;
+      
       if (!Homie.getMqttClient().connected() || WiFi.status() != 3) {
         if (0 == problemDetected) {
           if (WiFi.status() != 3) {
@@ -91,24 +94,26 @@ void fixWiFi() {
           Homie.getLogger() << "Connectivity in problematic state --> " << problemCause << endl;
           problemDetected = millis();
         }
-        else if ((millis() - problemDetected) > 60000 && problemCount < 5) {
+        else if ((millis() - problemDetected) > 120000 && (problemCount >= 5)) {
+          Homie.getLogger() << "Connectivity in problematic state --> This remained for 10 minutes. Rebooting!" << endl;
+          Homie.reboot();
+        }
+        else if ((millis() - problemDetected) > 120000 && problemCount < 5) {
           problemCount = (problemCount + 1);
-          Homie.getLogger() << "Connectivity in problematic state --> " << problemCause << "/n This remained for 60 seconds. Disconnecting WiFi to start over." << endl;
+          Homie.getLogger() << "Connectivity in problematic state --> " << problemCause << "/n This remained for 2 minutes. Disconnecting WiFi to start over." << endl;
           problemDetected = 0;
-          problemCount = 0;
           problemCause = "";
           WiFi.disconnect();
         }
-        else if ((millis() - problemDetected) > 60000 && (problemCount >= 5)) {
-          Homie.reboot();
-        }
       }
-      else if (Homie.getMqttClient().connected() || WiFi.status() == 3) {
+      else if (problemCount != 0 && Homie.getMqttClient().connected() || WiFi.status() == 3) {
         problemCount = 0;
+        ArduinoOTA.setHostname(Homie.getConfiguration().deviceId);
+        ArduinoOTA.begin();
       }
     }
+    WiFifix = millis();
   }
-  WiFifix = millis();
 }
 
 
